89ed5159db69a5a8e13dba04b098e21200added0
assilbekov <askhat.assilbekov@gmail.com>
2024-04-27 18:43:52 -0300

    use created blocks

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,14 @@
 package main
 
+import "factory-method/pkg"
+
+var computerTypes = []string{pkg.LaptopType, pkg.ServerType, pkg.ClientType, "unknown"}
+
 func main() {
-	//
+	for _, typeName := range computerTypes {
+		computer := pkg.New(typeName)
+		if computer != nil {
+			computer.PrintDetails()
+		}
+	}
 }
